1168b264503448f42e2e659cdbbb9ec506b42c2e
ThomasVanhoutte <thomas.vanhoutte@tvh.com>
2017-10-17 11:25:20 +0200

    EX2 Changing implementation of classes.

--- src/Library.ts
+++ src/Library.ts
@@ -1,3 +1,21 @@
 class Library {
+    items:Array<Item> = [];
 
+    constructor(){
+        let matrix  = new Movie("Matrix","thriller","tell em some",16,"PG");
+        let mobyDick = new Book("Moby Dick",new Author("Herman Malville"),"Drama","About a big fish");
+
+        this.items.push(matrix);
+        this.items.push(mobyDick);
+    }
+
+    static fromJSON(data: any) : Library {
+        return null
+    }
+
+    publishItems():void{
+
+        let itemContainer: HTMLElement = document.getElementById("items");
+        this.items.forEach(item => item.render(itemContainer));
+    }
 }
